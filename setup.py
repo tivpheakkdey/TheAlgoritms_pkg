0d7658cb75b08af9a624341c36316e87af602be5
vtiv <vtiv@deakin.edu.au>
2020-09-29 20:21:43 +1000

    changed requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TheAlgorithms-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="TheAlgorithms_pkg", # Replace with your own username
     version="0.0.2",
     author="Vireakpheakkdey Tiv",
     author_email="vtiv@deakin.edu.au",
@@ -12,15 +12,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tivpheakkdey/TheAlgoritms_pkg",
-    download_url = "https://github.com/tivpheakkdey/TheAlgoritms_pkg/archive/0.0.2.tar.gz",
+    download_url = "https://github.com/tivpheakkdey/TheAlgoritms_pkg/archive/0.0.3.tar.gz",
     packages=setuptools.find_packages(),
     install_requires=[
-          'string',
       ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3',
+    python_requires='>=3.6',
 )
