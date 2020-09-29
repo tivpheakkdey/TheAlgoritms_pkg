2f213eb1190cbee4bb447017f976282db16dc9d4
vtiv <vtiv@deakin.edu.au>
2020-09-29 19:52:39 +1000

    modified setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TheAlgorithms-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Vireakpheakkdey Tiv",
     author_email="vtiv@deakin.edu.au",
     description="A sample package to demonstrate how the packaging work",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TheAlgorithms/Python",
+    url="https://github.com/tivpheakkdey/TheAlgoritms_pkg",
+    download_url = "https://github.com/tivpheakkdey/TheAlgoritms_pkg/archive/0.0.1.tar.gz",
     packages=setuptools.find_packages(),
     install_requires=[
           'string',
